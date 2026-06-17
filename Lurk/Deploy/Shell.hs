ae37953ec98402be2a1962679b6247e6536691cc
Fernando Romay <fernando.romay@ruzaani.com>
2026-06-17 16:09:12 -0600

    Implement new interface in ShellProvider and genericize binary paths

--- Lurk/Deploy/Shell.hs
+++ Lurk/Deploy/Shell.hs
@@ -7,11 +7,12 @@ module Lurk.Deploy.Shell
 import Lurk.Deploy
 import GHC.Generics (Generic)
 import Data.Aeson (FromJSON)
-import System.Process (readProcessWithExitCode)
+import System.Process (readProcessWithExitCode, readProcess, callProcess)
 import System.Exit (ExitCode(..))
 
 data ShellConfig = ShellConfig
-    { script :: FilePath
+    { script       :: FilePath
+    , service_name :: String
     } deriving (Show, Generic)
 
 instance FromJSON ShellConfig
@@ -19,9 +20,16 @@ instance FromJSON ShellConfig
 data ShellProvider = ShellProvider ShellConfig
 
 instance DeployProvider ShellProvider where
+    setup (ShellProvider cfg) = runStep cfg "setup" Nothing
     validate (ShellProvider cfg) = runStep cfg "validate" Nothing
-    package (ShellProvider cfg)  = runStep cfg "package" Nothing
-    transfer (ShellProvider cfg) mEnvPath = runStep cfg "transfer" mEnvPath
+    
+    package (ShellProvider cfg) = do
+        putStrLn "Packaging project..."
+        callProcess "cabal" ["build", "--minimize"]
+        out <- readProcess "cabal" ["list-bin", service_name cfg] ""
+        pure $ Right (init out)
+
+    transfer (ShellProvider cfg) binaryPath mEnvPath = runStep cfg "transfer" (Just binaryPath)
     activate (ShellProvider cfg) = runStep cfg "activate" Nothing
     rollback (ShellProvider cfg) = runStep cfg "rollback" Nothing
 
