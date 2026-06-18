5d43c34f28b225eeb5e79c7878a738e64ed156a5
Fernando Romay <fernando.romay@ruzaani.com>
2026-06-18 01:37:42 -0600

    Add forEach and forEachWithIndex to Lurk.Html

--- Lurk/Html.hs
+++ Lurk/Html.hs
@@ -4,6 +4,7 @@ module Lurk.Html where
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.String (IsString)
+import Data.Foldable (toList)
 
 -- | The core LURK Html type. 
 -- For now, it's a wrapper around Text, but it can be expanded into a DOM tree later.
@@ -33,3 +34,12 @@ preEscapedToHtml = Html
 -- | Combine multiple HTML nodes
 concatHtml :: [Html] -> Html
 concatHtml = Html . T.concat . map renderHtml
+
+-- | Map over a foldable structure and concatenate the resulting Html.
+-- Synonym for @foldMap@ specialized to Html.
+forEach :: Foldable t => t a -> (a -> Html) -> Html
+forEach = flip foldMap
+
+-- | Like 'forEach', but the function also receives the element's 1-based index.
+forEachWithIndex :: Foldable t => t a -> (Int -> a -> Html) -> Html
+forEachWithIndex xs f = foldMap (\(i, x) -> f i x) (zip [1..] (toList xs))
