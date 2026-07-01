3f5020ff6bc663227071e9918549721ba6dd44f5
Fernando Romay <fernando.romay@ruzaani.com>
2026-06-30 22:39:59 -0600

    refactor(db): rewrite TH to generate Core instances
    TH now generates Core.FromRow (field threading) and Core.ToRow (toField
    per field) instead of sqlite-simple instances.

--- Lurk/DB/TH.hs
+++ Lurk/DB/TH.hs
@@ -1,21 +1,5 @@
 -- | Template Haskell deriving for 'FromRow' and 'ToRow' instances.
--- Generates sqlite-simple compatible instances.
---
--- @
--- data Post = Post
---   { postId       :: Int
---   , postTitle    :: Text
---   , postContent  :: Maybe Text
---   , postAuthorId :: Int
---   } deriving (Show, Generic)
---
--- deriveFromRow ''Post
--- -- Generates FromRow instance:
--- --   postId       -> post_id
--- --   postTitle    -> post_title
--- --   postContent  -> post_content
--- --   postAuthorId -> post_author_id
--- @
+-- Generates instances for Lurk.DB.Core typeclasses.
 module Lurk.DB.TH
     ( deriveFromRow
     , camelToSnake
@@ -24,8 +8,7 @@ module Lurk.DB.TH
 
 import Language.Haskell.TH
 import Data.Char (toLower, isUpper)
-import Database.SQLite.Simple (FromRow(..), ToRow(..), field)
-import qualified Database.SQLite.Simple as SQLite
+import qualified Lurk.DB.Core as Core
 
 -- | Derive 'FromRow' and 'ToRow' for a record type.
 -- Convention: strip the type name as prefix, convert camelCase to snake_case.
@@ -35,36 +18,60 @@ deriveFromRow typeName = do
     case info of
         TyConI (DataD _ _ _ _ [RecC _ fields] _) -> do
             let tyName = nameBase typeName
-                colNames = map (camelToSnake . stripPrefix tyName . nameBase . fst3) fields
                 fieldTypes = map (\(_, _, typ) -> typ) fields
-            fromRowDec <- deriveFromRowInstance typeName fieldTypes colNames
-            toRowDec <- deriveToRowInstance typeName fieldTypes colNames
+            fromRowDec <- deriveFromRowInstance typeName fieldTypes
+            toRowDec <- deriveToRowInstance typeName fieldTypes
             pure [fromRowDec, toRowDec]
         _ -> fail $ "deriveFromRow: " ++ nameBase typeName ++ " must be a record type with a single constructor"
-  where
-    fst3 (a, _, _) = a
 
--- | Generate a FromRow instance using sqlite-simple's field combinator.
-deriveFromRowInstance :: Name -> [Type] -> [String] -> Q Dec
-deriveFromRowInstance typeName fields _colNames = do
-    let instanceType = AppT (ConT ''FromRow) (ConT typeName)
+-- | Generate a FromRow instance using Core.field with explicit threading.
+--
+-- Generates:
+-- @
+-- instance FromRow Post where
+--     fromRow vals = do
+--         (x1, r1) <- Core.field vals
+--         (x2, r2) <- Core.field r1
+--         (x3, _)  <- Core.field r2
+--         pure (Post x1 x2 x3)
+-- @
+deriveFromRowInstance :: Name -> [Type] -> Q Dec
+deriveFromRowInstance typeName fields = do
+    let instanceType = AppT (ConT ''Core.FromRow) (ConT typeName)
         fieldCount = length fields
-        fieldBodies = replicate fieldCount (AppE (VarE 'field) (VarE (mkName "_")))
-        doBlock = DoE Nothing (map NoBindS fieldBodies)
-        fromRowClause = Clause [] (NormalB doBlock) []
+        varNames = map (\i -> mkName ("x" ++ show i)) [1..fieldCount]
+        -- Generate: (x1, r1) <- Core.field vals
+        --           (x2, r2) <- Core.field r1
+        --           ...
+        --           (xN, _)  <- Core.field rN-1
+        bindStmts = zipWith3 mkBind varNames restNames prevNames
+        mkBind vn rn pn = BindS (TupP [VarP vn, VarP rn]) (AppE (VarE 'Core.field) (VarE pn))
+        restNames = map (\i -> mkName ("r" ++ show i)) [1..fieldCount]
+        prevNames = mkName "vals" : init restNames
+        -- pure (Constructor x1 x2 ... xN)
+        pureExpr = AppE (VarE 'pure) (AppE (ConE typeName) (TupE (map (Just . VarE) varNames)))
+        doBlock = DoE Nothing (bindStmts ++ [NoBindS pureExpr])
+        fromRowClause = Clause [VarP (mkName "vals")] (NormalB doBlock) []
     pure $ InstanceD Nothing [] instanceType
-        [ FunD 'fromRow [fromRowClause]
+        [ FunD 'Core.fromRow [fromRowClause]
         ]
 
--- | Generate a ToRow instance using sqlite-simple's toRow on tuples.
-deriveToRowInstance :: Name -> [Type] -> [String] -> Q Dec
-deriveToRowInstance typeName fields _colNames = do
-    let instanceType = AppT (ConT ''ToRow) (ConT typeName)
+-- | Generate a ToRow instance using Core.toField on each field.
+--
+-- Generates:
+-- @
+-- instance ToRow Post where
+--     toRow (Post x1 x2 x3) = [Core.toField x1, Core.toField x2, Core.toField x3]
+-- @
+deriveToRowInstance :: Name -> [Type] -> Q Dec
+deriveToRowInstance typeName fields = do
+    let instanceType = AppT (ConT ''Core.ToRow) (ConT typeName)
         fieldNames = map (\i -> mkName ("x" ++ show i)) [1..length fields]
-        tupleExpr = AppE (VarE 'SQLite.toRow) (TupE (map (Just . VarE) fieldNames))
-        toRowClause = Clause [ConP typeName [] (map VarP fieldNames)] (NormalB tupleExpr) []
+        -- [Core.toField x1, Core.toField x2, ...]
+        listExpr = ListE (map (\fn -> AppE (VarE 'Core.toField) (VarE fn)) fieldNames)
+        toRowClause = Clause [ConP typeName [] (map VarP fieldNames)] (NormalB listExpr) []
     pure $ InstanceD Nothing [] instanceType
-        [ FunD 'SQLite.toRow [toRowClause]
+        [ FunD 'Core.toRow [toRowClause]
         ]
 
 -- | Convert camelCase to snake_case.
