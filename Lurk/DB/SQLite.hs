36443601fa2836e795658315809ac089a9c01b02
Fernando Romay <fernando.romay@ruzaani.com>
2026-06-30 22:43:08 -0600

    refactor(db): rewrite SQLite provider to use direct-sqlite
    Bridges Core.SqlValue <-> Direct.SQLData via bindSqliteParams and
    sqlDataToSqlValue. readAllRows uses Core.FromRow. Also removed
    unused dynamic dep, added direct-sqlite >= 2.3.

--- Lurk/DB/SQLite.hs
+++ Lurk/DB/SQLite.hs
@@ -7,9 +7,14 @@ module Lurk.DB.SQLite
 
 import Data.Pool (Pool, withResource, destroyAllResources)
 import qualified Data.Pool as Pool
+import Data.Int (Int64)
+import Data.Text (Text)
+import qualified Data.Text as T
 import Database.SQLite.Simple (Connection)
 import qualified Database.SQLite.Simple as SQLite
-import Lurk.DB.Core (DatabaseProvider(..))
+import qualified Database.SQLite.Simple.Internal as Internal
+import qualified Database.SQLite3 as Direct
+import Lurk.DB.Core (DatabaseProvider(..), Query(..), FromRow(..), ToRow(..), SqlValue(..))
 import Lurk.DB.Config (DbConfig(..))
 
 -- | SQLite database provider.
@@ -28,8 +33,63 @@ newSqlitePool cfg = do
     pure (SqliteProvider pool)
 
 instance DatabaseProvider SqliteProvider where
-    query (SqliteProvider pool) sql params = withResource pool $ \conn ->
-        SQLite.query conn sql params
-    execute (SqliteProvider pool) sql params = withResource pool $ \conn ->
-        SQLite.execute conn sql params
+    query (SqliteProvider pool) q params = withResource pool $ \conn -> do
+        let db = Internal.connectionHandle conn
+        stmt <- Direct.prepare db (unQuery q)
+        bindSqliteParams stmt (toRow params)
+        rows <- readAllRows stmt
+        Direct.finalize stmt
+        pure rows
+
+    execute (SqliteProvider pool) q params = withResource pool $ \conn -> do
+        let db = Internal.connectionHandle conn
+        stmt <- Direct.prepare db (unQuery q)
+        bindSqliteParams stmt (toRow params)
+        _ <- Direct.step stmt
+        n <- Direct.changes db
+        Direct.finalize stmt
+        pure (fromIntegral n)
+
     closeProvider (SqliteProvider pool) = destroyAllResources pool
+
+----------------------------------------------------------------------
+-- Bridge: Core.SqlValue → direct-sqlite binding
+----------------------------------------------------------------------
+
+bindSqliteParams :: Direct.Statement -> [SqlValue] -> IO ()
+bindSqliteParams stmt = mapM_ bind1 . zip [1..]
+  where
+    bind1 (i, SqlNull)          = Direct.bindNull stmt i
+    bind1 (i, SqlInt n)         = Direct.bindInt64 stmt i (fromIntegral n)
+    bind1 (i, SqlDouble d)      = Direct.bindDouble stmt i d
+    bind1 (i, SqlText t)        = Direct.bindText stmt i t
+    bind1 (i, SqlByteString bs) = Direct.bindBlob stmt i bs
+    bind1 (i, SqlBool b)        = Direct.bindInt64 stmt i (if b then 1 else 0)
+    bind1 (i, SqlLocalTime t)   = Direct.bindText stmt i (T.pack (show t))
+
+----------------------------------------------------------------------
+-- Bridge: direct-sqlite SQLData → Core.SqlValue
+----------------------------------------------------------------------
+
+sqlDataToSqlValue :: Direct.SQLData -> SqlValue
+sqlDataToSqlValue Direct.SQLNull        = SqlNull
+sqlDataToSqlValue (Direct.SQLInteger n) = SqlInt (fromIntegral n)
+sqlDataToSqlValue (Direct.SQLFloat d)   = SqlDouble d
+sqlDataToSqlValue (Direct.SQLText t)    = SqlText t
+sqlDataToSqlValue (Direct.SQLBlob bs)   = SqlByteString bs
+
+----------------------------------------------------------------------
+-- Read rows using Core.FromRow
+----------------------------------------------------------------------
+
+readAllRows :: FromRow row => Direct.Statement -> IO [row]
+readAllRows stmt = do
+    result <- Direct.step stmt
+    case result of
+        Direct.Row -> do
+            sqlDataList <- Direct.columns stmt
+            let values = map sqlDataToSqlValue sqlDataList
+            case fromRow values of
+                Left err -> error $ "row parse error: " ++ err
+                Right row -> (row :) <$> readAllRows stmt
+        Direct.Done -> pure []
